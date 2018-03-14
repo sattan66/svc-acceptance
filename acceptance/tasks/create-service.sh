c64b09935d24703cde1de105ab9ecb9284ae96a5
Sattanathan Selvanathan <sattan66@gmail.com>
2018-03-14 16:36:28 +0530

    Update create-service.sh

--- acceptance/tasks/create-service.sh
+++ acceptance/tasks/create-service.sh
@@ -2,12 +2,18 @@
 
 set -xe
 
+echo "hello"
+
 cf api $API --skip-ssl-validation
 
 cf login -u $USER -p $PASSWORD -o "$ORG" -s "$SPACE"
 
+echo "cf login done"
+
 SERVICE_INSTANCE=smk-test-$SERVICE
 cf create-service $SERVICE $PLAN $SERVICE_INSTANCE > changes-status.txt
+
+echo " cf service created"
 
 if [[ $? -ne 0 ]]; then
   echo "Service creation failed"
